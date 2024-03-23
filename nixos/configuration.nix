2ced145a4bab23ae50c2470b41160484afa136d5
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2024-03-23 16:31:42 +0100

    Remove depecrecated public keys config option

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,9 +50,6 @@
       substituters = [
         "https://cache.nixos.org/"
       ];
-      trustedPublicKeys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      ];
     };
   };
 
