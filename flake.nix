fe01e8d2b36919b5d75b4573eb0a14c5c0a578e5
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2024-03-24 05:30:47 +0100

    Fix main flake

--- flake.nix
+++ flake.nix
@@ -2,38 +2,23 @@
   description = "Lasse's personal nix config";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Additional flakes
     hardware.url = "github:nixos/nixos-hardware";
     nix-colors.url = "github:misterio77/nix-colors";
     nixos-conf-editor.url = "github:snowfallorg/nixos-conf-editor";
     nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    hardware,
-    nix-colors,
-    nixos-conf-editor,
-    nix-alien
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
+  outputs = { self, nixpkgs, home-manager, hardware, nix-colors, nixos-conf-editor, nix-alien, ... } @ inputs: {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#hostname'
     nixosConfigurations = {
       herobox-nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
+        specialArgs = { inherit inputs; };
         # main nixos configuration file
-        modules = [./nixos/configuration.nix];
+        modules = [ ./nixos/configuration.nix ];
       };
     };
   };
