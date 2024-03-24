27a92e30a8c6bec58a7059ffa704542c6851244b
Lasse Siemoneit <lasse.siemoneit@gmail.com>
2024-03-24 04:58:13 +0100

    Add nix-alien

--- nixos/modules/nix-alien.nix
+++ nixos/modules/nix-alien.nix
@@ -1,15 +1,22 @@
-{ ... }:
-
-let
-  nix-alien-pkgs = import (
-    builtins.fetchTarball "https://github.com/thiagokokada/nix-alien/tarball/master"
-  ) { };
-in
 {
-  environment.systemPackages = with nix-alien-pkgs; [
-    nix-alien
-  ];
+  description = "nix-alien-on-nixos";
 
-  # Optional, but this is needed for `nix-alien-ld` command
-  programs.nix-ld.enable = true;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nix-alien.url = "github:thiagokokada/nix-alien";
+
+  outputs = { self, nixpkgs, nix-alien }: {
+      nixosConfigurations.nix-alien-desktop = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux"; # or aarch64-linux
+        specialArgs = { inherit self system; };
+        modules = [
+          ({ self, system, ... }: {
+            environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [
+              nix-alien
+            ];
+            # Optional, needed for `nix-alien-ld`
+            programs.nix-ld.enable = true;
+          })
+        ];
+    };
+  };
 }
